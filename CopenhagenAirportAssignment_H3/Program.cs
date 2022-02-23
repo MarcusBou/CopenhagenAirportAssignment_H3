a5f5315495ac33147957ceb33297d252b92acdcf
Marcus Damgaard Bournonwille (MARC8847) <marc8847@zbc.dk>
2022-02-23 15:51:44 +0100

    Initial commit

---
+++ CopenhagenAirportAssignment_H3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CopenhagenAirportAssignment_H3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
